893c1ba0263b02e9a00af335d9d54f420d011f8b
Pieter Slabbert <blob626@gmail.com>
2013-07-15 19:35:25 +0200

    refactor to remove global variables
    remove first_file and last_file
    these are now given in the rnadom_names function
    it defaults to using the config file if no files are provided

--- core/crew/generation/name/name-generator.py
+++ core/crew/generation/name/name-generator.py
@@ -7,9 +7,6 @@ import configparser
 config = configparser.ConfigParser()
 config.read('name.config')
 
-first_file = config['Files']['First']
-last_file = config['Files']['Last']
-
 def name_list(filename):
     with open(filename) as in_file:
         return [line.strip() for line in in_file]
@@ -19,7 +16,11 @@ def random_name(first_names, last_names):
     last = random.choice(last_names)
     return {"first": first, "last": last}
     
-def random_names(num, first=first_file, last=last_file):
+def random_names(num, first = None, last = None):
+    if first is None:
+        first = config['Files']['First']
+    if last is None:
+        last = config['Files']['Last']    
     first_names = name_list(first)
     last_names = name_list(last)
     return [random_name(first_names, last_names) for i in range(num)]    
