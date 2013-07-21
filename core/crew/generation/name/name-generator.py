cbf52ea5a55110de8fa31db3d7e8ba14f56f2e00
Pieter Slabbert <blob626@gmail.com>
2013-07-21 21:13:08 +0200

    create Name class and NameGenerator class
    The Name class just stores a first and last name
    NameGenerator can generate a list of random names

--- core/crew/generation/name/name-generator.py
+++ core/crew/generation/name/name-generator.py
@@ -7,23 +7,38 @@ import configparser
 config = configparser.ConfigParser()
 config.read('name.config')
 
+class Name():
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+class NameGenerator():
+    def __init__(self, first_names, last_names):
+        self.first_names = first_names
+        self.last_names = last_names
+
+        
+    def random_name(self):
+        first = random.choice(first_names)
+        last = random.choice(last_names)
+        return Name(first, last)
+
+        
+    def random_names(self, num):
+        return [self.random_name() for i in range(num)]
+
+        
 def name_list(filename):
     with open(filename) as in_file:
         return [line.strip() for line in in_file]
 
-def random_name(first_names, last_names):
-    first = random.choice(first_names)
-    last = random.choice(last_names)
-    return {"first": first, "last": last}
-    
-def random_names(num, first = None, last = None):
-    if first is None:
-        first = config['Files']['First']
-    if last is None:
-        last = config['Files']['Last']    
-    first_names = name_list(first)
-    last_names = name_list(last)
-    return [random_name(first_names, last_names) for i in range(num)]    
-    
-names = random_names(3)
-print(json.dumps(names))
+
+
+first_names = name_list(config['Files']['First'])
+last_names = name_list(config['Files']['Last'])
+nameGenerator = NameGenerator(first_names, last_names)
+
+names = nameGenerator.random_names(4)
+#print(json.dumps(names))
+for i in names:
+    print(i.first + " " + i.last)
